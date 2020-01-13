bf4c2b6289acd2a30522e31b2afcd05207f49b5e
joe <joe@changingtec.com>
2020-01-13 13:51:08 +0800

    加入專案檔案。

---
+++ ACGMapping/SpaMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ACGMapping
+{
+    public class SpaMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public SpaMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next.Invoke(context);
+            if (context.Response.StatusCode == 404 &&
+                !System.IO.Path.HasExtension(context.Request.Path.Value) &&
+                !context.Request.Path.Value.StartsWith("/api"))
+            {
+                context.Request.Path = "~/Index.html";
+                context.Response.StatusCode = 200;
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class SpaMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseSpaMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SpaMiddleware>();
+        }
+    }
+}
